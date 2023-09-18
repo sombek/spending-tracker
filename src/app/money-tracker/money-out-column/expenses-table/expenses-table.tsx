e91e6eea4a1b5af7d37b53aa34691a276b6a2b79
Abdullah Hashim <Abdullah97hashim@gmail.com>
2023-09-19 00:58:00 +0300

    feat: add responsiveness for expenses tables

--- src/app/money-tracker/money-out-column/expenses-table/expenses-table.tsx
+++ src/app/money-tracker/money-out-column/expenses-table/expenses-table.tsx
@@ -3,19 +3,34 @@ import "ag-grid-community/styles/ag-theme-alpine.css";
 import "reactflow/dist/style.css";
 import CategoryTable from "app/money-tracker/money-out-column/expenses-table/category-table";
 import { MultiPaymentBreakdown } from "infrastructure/backend-service";
+import { useEffect, useMemo, useState } from "react";
 
 const ExpensesTable = (props: {
   setMultiPayments: (data: MultiPaymentBreakdown[]) => void;
   multiPayments: MultiPaymentBreakdown[];
 }) => {
+  // get page width
+  const [width, setWidth] = useState(window.innerWidth);
+  // if there is enough space, show 3 columns, otherwise show 2 columns, otherwise show 1 column
+  const getColumns = useMemo(() => {
+    if (width > 1150) return 3;
+    else if (width > 935) return 2;
+    return 1;
+  }, [width]);
+
+  const [columns, setColumns] = useState(getColumns);
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      setColumns(getColumns);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [getColumns, width]);
+
   return (
     <>
-      <div
-        className="grid grid-cols-2 gap-4"
-        style={{
-          maxWidth: 460,
-        }}
-      >
+      <div className={`grid grid-cols-${columns} gap-4`}>
         {props.multiPayments.map((payment, index) => {
           const title = payment.title;
           if (
