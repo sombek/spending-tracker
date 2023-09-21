c8fa7c68375b6f555b7f0f6b2a3feadbf65b798b
Abdullah Hashim <Abdullah97hashim@gmail.com>
2023-09-21 16:12:38 +0300

    🚧WIP: drag and drop for tables, issue with table height

--- src/app/money-tracker/right-side/category-table.tsx
+++ src/app/money-tracker/right-side/category-table.tsx
@@ -34,12 +34,12 @@ const CategoryTable = (props: {
   }
 
   return (
-    <>
+    <div>
       <span className="inline-flex items-center rounded-md bg-indigo-50 px-2 py-1 text-xs font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10">
         <h2>{props.title}</h2>
       </span>
 
-      <div className="mt-2" style={{ maxWidth: 250 }}>
+      <div style={{ maxWidth: 250, marginTop: 10 }}>
         <DataTable
           scrollRef={rightSideScrollContext}
           tableRef={props.tableRef}
@@ -52,7 +52,7 @@ const CategoryTable = (props: {
           onAfterRemoveRow={updateCategoryPurchases}
         />
       </div>
-    </>
+    </div>
   );
 };
 
--- src/app/money-tracker/right-side/right-side.tsx
+++ src/app/money-tracker/right-side/right-side.tsx
@@ -1,7 +1,16 @@
-import { RefObject, useEffect, useMemo, useState } from "react";
+import {
+  createRef,
+  RefObject,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { HotTable } from "@handsontable/react";
 import { MultiPaymentBreakdown } from "infrastructure/backend-service";
 import CategoryTable from "app/money-tracker/right-side/category-table";
+import GridLayout from "react-grid-layout";
+import { RightSideScrollContext } from "app/money-tracker/money-tracker";
 
 const RightSide = (props: {
   tableRefs: {
@@ -10,32 +19,157 @@ const RightSide = (props: {
   multiPayments: MultiPaymentBreakdown[];
   setMultiPayments: (data: MultiPaymentBreakdown[]) => void;
 }) => {
-  // get page width
-  const [width, setWidth] = useState(window.innerWidth);
-  // if there is enough space, show 3 columns, otherwise show 2 columns, otherwise show 1 column
-  // formula left column = variable 300
-  // 300 + 250 = 1 cols
-  // 300 + 250 + 250 = 2 cols
-  // 300 + 250 + 250 + 250 = 3 cols
-  const leftColumnWidth = 300;
-  const getColumns = useMemo(() => {
-    if (width > leftColumnWidth + 250 * 6) return "grid-cols-6";
-    else if (width > leftColumnWidth + 250 * 5) return "grid-cols-5";
-    else if (width > leftColumnWidth + 250 * 4) return "grid-cols-4";
-    else if (width > leftColumnWidth + 250 * 3) return "grid-cols-3";
-    else if (width > leftColumnWidth + 250 * 2) return "grid-cols-2";
-    else return "grid-cols-1";
-  }, [width]);
+  const elementRef = useContext(RightSideScrollContext);
+  const [canvasWidth, setCanvasWidth] = useState(
+    // @ts-ignore
+    elementRef?.current?.clientWidth - 20,
+  );
+  const [numberOfColumns, setNumberOfColumns] = useState(1);
 
   useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth);
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, [width]);
+    if (!elementRef) return;
+    if (!elementRef.current) return;
+    const resizeObserver = new ResizeObserver(() => {
+      if (!elementRef.current) return;
+      setCanvasWidth(elementRef.current.clientWidth - 20);
+      console.log(
+        Math.floor(elementRef.current.clientWidth / 250),
+        "number of columns",
+      );
+      setNumberOfColumns(Math.floor(elementRef.current.clientWidth / 250));
+
+      const initialLayout: {
+        i: string;
+        x: number;
+        y: number;
+        w: number;
+        h: number;
+      }[] = [];
+
+      const lastPosition = { x: 0, y: 0 };
+      props.multiPayments.forEach((payment) => {
+        if (payment.title == "") return;
+        const width = 1;
+
+        // how many rows does this category have
+        const numberOfItems = payment.purchases.length;
+        // (numberOfItems+1 head row)*23+ 4 extra + 24 for the label + 10 for the marginTop
+        const height = (numberOfItems + 1) * 23 + 4 + 24 + 10 + 10;
+
+        // make it next to each other
+        initialLayout.push({
+          i: payment.title,
+          x: lastPosition.x,
+          y: lastPosition.y,
+          w: width,
+          h: height,
+        });
+        lastPosition.x += width;
+        if (lastPosition.x >= numberOfColumns) {
+          lastPosition.x = 0;
+          lastPosition.y += height;
+        }
+      });
+      setLayout(initialLayout);
+      console.log(initialLayout);
+    });
+    resizeObserver.observe(elementRef.current);
+    return () => resizeObserver.disconnect(); // clean up
+  }, [elementRef, numberOfColumns, props.multiPayments]);
+
+  const [layout, setLayout] = useState<
+    { i: string; x: number; y: number; w: number; h: number }[]
+  >(() => {
+    const initialLayout: {
+      i: string;
+      x: number;
+      y: number;
+      w: number;
+      h: number;
+    }[] = [];
+
+    const lastPosition = { x: 0, y: 0 };
+    props.multiPayments.forEach((payment) => {
+      if (payment.title == "") return;
+      const width = 1;
+
+      // how many rows does this category have
+      const numberOfItems = payment.purchases.length;
+      // (numberOfItems+1 head row)*23+ 4 extra + 24 for the label + 10 for the marginTop
+      const height = (numberOfItems + 1) * 23 + 4 + 24 + 10 + 10;
+
+      // make it next to each other
+      initialLayout.push({
+        i: payment.title,
+        x: lastPosition.x,
+        y: lastPosition.y,
+        w: width,
+        h: height,
+      });
+      lastPosition.x += width;
+      if (lastPosition.x >= numberOfColumns) {
+        lastPosition.x = 0;
+        lastPosition.y += height;
+      }
+    });
+    return initialLayout;
+  });
+
+  // calculateLayout();
+
+  const categoryTableRefs = useRef(
+    props.multiPayments.reduce(
+      (acc, payment) => {
+        return {
+          ...acc,
+          [payment.title]: createRef<HTMLDivElement>(),
+        };
+      },
+      {} as { [key: string]: RefObject<HTMLDivElement> },
+    ),
+  );
+
+  // function calculateLayout() {
+  //   props.multiPayments.forEach((payment) => {
+  //     if (payment.title == "") return;
+  //     const width = 1;
+  //
+  //     // how many rows does this category have
+  //     const numberOfItems = payment.purchases.length;
+  //     // (numberOfItems+1 head row)*23+ 4 extra + 24 for the label + 10 for the marginTop
+  //     const height = (numberOfItems + 1) * 23 + 4 + 24 + 10 + 10;
+  //
+  //     // make it next to each other
+  //     layout.push({
+  //       i: payment.title,
+  //       x: lastPosition.x,
+  //       y: lastPosition.y,
+  //       w: width,
+  //       h: height,
+  //     });
+  //     setLayout(layout);
+  //     lastPosition.x += width;
+  //     if (lastPosition.x >= numberOfColumns) {
+  //       lastPosition.x = 0;
+  //       lastPosition.y += height + 10;
+  //     }
+  //   });
+  // }
 
   return (
-    <div className={`grid ${getColumns} gap gap-2`}>
-      {props.multiPayments.map((payment, index) => {
+    <GridLayout
+      className="layout"
+      layout={layout}
+      width={canvasWidth}
+      cols={numberOfColumns}
+      rowHeight={1}
+      margin={[0, 0]}
+      isResizable={false}
+      onLayoutChange={(layout) => {
+        setLayout(layout);
+      }}
+    >
+      {props.multiPayments.map((payment) => {
         const title = payment.title;
         if (
           title === null ||
@@ -47,9 +181,10 @@ const RightSide = (props: {
 
         return (
           <div
-            key={index}
+            ref={categoryTableRefs.current[payment.title]}
+            key={title}
             style={{
-              width: 250,
+              maxWidth: 250,
               paddingLeft: 10,
               paddingRight: 10,
             }}
@@ -63,7 +198,7 @@ const RightSide = (props: {
           </div>
         );
       })}
-    </div>
+    </GridLayout>
   );
 };
 
