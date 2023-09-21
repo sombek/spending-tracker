3cb61d5a3c2bf2c9d928cdb0e7af77b508271653
Abdullah Hashim <Abdullah97hashim@gmail.com>
2023-09-21 20:06:28 +0300

    🚧WIP: clean up for the code

--- src/app/money-tracker/right-side/right-side.tsx
+++ src/app/money-tracker/right-side/right-side.tsx
@@ -1,6 +1,7 @@
 import {
   createRef,
   RefObject,
+  useCallback,
   useContext,
   useEffect,
   useRef,
@@ -9,7 +10,7 @@ import {
 import { HotTable } from "@handsontable/react";
 import { MultiPaymentBreakdown } from "infrastructure/backend-service";
 import CategoryTable from "app/money-tracker/right-side/category-table";
-import GridLayout from "react-grid-layout";
+import GridLayout, { Layout } from "react-grid-layout";
 import { RightSideScrollContext } from "app/money-tracker/money-tracker";
 
 const RightSide = (props: {
@@ -19,79 +20,20 @@ const RightSide = (props: {
   multiPayments: MultiPaymentBreakdown[];
   setMultiPayments: (data: MultiPaymentBreakdown[]) => void;
 }) => {
-  const elementRef = useContext(RightSideScrollContext);
-  const [canvasWidth, setCanvasWidth] = useState(
-    // @ts-ignore
-    elementRef?.current?.clientWidth - 20,
-  );
-  const [numberOfColumns, setNumberOfColumns] = useState(1);
+  const rightSideScrollElement = useContext(RightSideScrollContext);
+  const [canvasWidth, setCanvasWidth] = useState<number | null>(null);
+  const [numberOfColumns, setNumberOfColumns] = useState<number | null>(null);
+  const [isInitialized, setIsInitialized] = useState(false);
+  // initialize the layout
+  const initialLayout = useCallback(() => {
+    if (numberOfColumns === null) return [];
 
-  useEffect(() => {
-    if (!elementRef) return;
-    if (!elementRef.current) return;
-    const resizeObserver = new ResizeObserver(() => {
-      if (!elementRef.current) return;
-      setCanvasWidth(elementRef.current.clientWidth - 20);
-      console.log(
-        Math.floor(elementRef.current.clientWidth / 250),
-        "number of columns",
-      );
-      setNumberOfColumns(Math.floor(elementRef.current.clientWidth / 250));
-
-      const initialLayout: {
-        i: string;
-        x: number;
-        y: number;
-        w: number;
-        h: number;
-      }[] = [];
-
-      const lastPosition = { x: 0, y: 0 };
-      props.multiPayments.forEach((payment) => {
-        if (payment.title == "") return;
-        const width = 1;
-
-        // how many rows does this category have
-        const numberOfItems = payment.purchases.length;
-        // (numberOfItems+1 head row)*23+ 4 extra + 24 for the label + 10 for the marginTop
-        const height = (numberOfItems + 1) * 23 + 4 + 24 + 10 + 10;
-
-        // make it next to each other
-        initialLayout.push({
-          i: payment.title,
-          x: lastPosition.x,
-          y: lastPosition.y,
-          w: width,
-          h: height,
-        });
-        lastPosition.x += width;
-        if (lastPosition.x >= numberOfColumns) {
-          lastPosition.x = 0;
-          lastPosition.y += height;
-        }
-      });
-      setLayout(initialLayout);
-      console.log(initialLayout);
-    });
-    resizeObserver.observe(elementRef.current);
-    return () => resizeObserver.disconnect(); // clean up
-  }, [elementRef, numberOfColumns, props.multiPayments]);
-
-  const [layout, setLayout] = useState<
-    { i: string; x: number; y: number; w: number; h: number }[]
-  >(() => {
-    const initialLayout: {
-      i: string;
-      x: number;
-      y: number;
-      w: number;
-      h: number;
-    }[] = [];
-
+    const initialLayout: Layout[] = [];
     const lastPosition = { x: 0, y: 0 };
+    const width = 1;
+
     props.multiPayments.forEach((payment) => {
       if (payment.title == "") return;
-      const width = 1;
 
       // how many rows does this category have
       const numberOfItems = payment.purchases.length;
@@ -112,10 +54,33 @@ const RightSide = (props: {
         lastPosition.y += height;
       }
     });
+    setIsInitialized(true);
     return initialLayout;
-  });
+  }, [numberOfColumns, props.multiPayments]);
 
-  // calculateLayout();
+  const [layout, setLayout] = useState<Layout[]>(() => initialLayout());
+
+  const initializeCanvasWidth = useCallback(() => {
+    if (!rightSideScrollElement) return;
+    if (!rightSideScrollElement.current) return;
+    setCanvasWidth(rightSideScrollElement.current.clientWidth - 20);
+    setNumberOfColumns(
+      Math.floor((rightSideScrollElement.current.clientWidth - 20) / 250),
+    );
+  }, [rightSideScrollElement]);
+
+  // observe the width of the canvas, and update the canvas width
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver(() => {
+      initializeCanvasWidth();
+      setLayout(initialLayout());
+    });
+
+    if (!rightSideScrollElement) return;
+    if (!rightSideScrollElement.current) return;
+    resizeObserver.observe(rightSideScrollElement.current);
+    return () => resizeObserver.disconnect(); // clean up
+  }, [rightSideScrollElement, initializeCanvasWidth, initialLayout]);
 
   const categoryTableRefs = useRef(
     props.multiPayments.reduce(
@@ -128,33 +93,14 @@ const RightSide = (props: {
       {} as { [key: string]: RefObject<HTMLDivElement> },
     ),
   );
-
-  // function calculateLayout() {
-  //   props.multiPayments.forEach((payment) => {
-  //     if (payment.title == "") return;
-  //     const width = 1;
-  //
-  //     // how many rows does this category have
-  //     const numberOfItems = payment.purchases.length;
-  //     // (numberOfItems+1 head row)*23+ 4 extra + 24 for the label + 10 for the marginTop
-  //     const height = (numberOfItems + 1) * 23 + 4 + 24 + 10 + 10;
-  //
-  //     // make it next to each other
-  //     layout.push({
-  //       i: payment.title,
-  //       x: lastPosition.x,
-  //       y: lastPosition.y,
-  //       w: width,
-  //       h: height,
-  //     });
-  //     setLayout(layout);
-  //     lastPosition.x += width;
-  //     if (lastPosition.x >= numberOfColumns) {
-  //       lastPosition.x = 0;
-  //       lastPosition.y += height + 10;
-  //     }
-  //   });
-  // }
+  if (canvasWidth === null || numberOfColumns === null || !isInitialized)
+    return (
+      <div>
+        <div className="flex justify-center items-center h-full w-full">
+          <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+        </div>
+      </div>
+    );
 
   return (
     <GridLayout
