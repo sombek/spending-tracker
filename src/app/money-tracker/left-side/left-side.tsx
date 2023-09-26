db8228bae21043cd39f0405e862e27017fce39e3
Abdullah Hashim <Abdullah97hashim@gmail.com>
2023-09-26 14:37:04 +0300

    add easter egg

--- src/app/money-tracker/left-side/left-side.tsx
+++ src/app/money-tracker/left-side/left-side.tsx
@@ -99,6 +99,17 @@ const LeftSide = (props: {
   const sumOfMoneyRemaining = useMemo(() => {
     return sumOfMoneyIn - sumOfMoneyOut;
   }, [sumOfMoneyOut, sumOfMoneyIn]);
+  const randomEmoji = useMemo(() => {
+    return ["💰", "💸", "💵", "💴", "💶", "💷"].sort(
+      () => Math.random() - 0.5,
+    )[0];
+  }, []);
+
+  const randomEmoji2 = useMemo(() => {
+    return ["💰", "💸", "💵", "💴", "💶", "💷"].sort(
+      () => Math.random() - 0.5,
+    )[0];
+  }, []);
 
   return (
     <>
@@ -134,7 +145,13 @@ const LeftSide = (props: {
         </div>
       </div>
       {/*Divider*/}
-      <hr className="my-2 border-green-800" />
+      <div className="flex items-center">
+        <div className="border-t border-green-800 flex-grow" />
+        <div className="px-3 text-gray-800 font-bold text-2xl">
+          {randomEmoji}
+        </div>
+        <div className="border-t  border-green-800 flex-grow" />
+      </div>
       <OneTimePayments
         scrollRef={leftSideScrollContext}
         tableRef={props.tableRefs.singlePayments}
@@ -159,7 +176,52 @@ const LeftSide = (props: {
         </div>
       </div>
       {/*Divider*/}
-      <hr className="my-2 border-green-800" />
+      <div className="flex items-center">
+        <div className="border-t border-green-800 flex-grow" />
+        <div
+          className="px-3 text-gray-800 font-bold text-2xl cursor-pointer"
+          onClick={() => {
+            // say a joke about money tracking
+            const moneyJokes = [
+              "Why did the dollar go to therapy? It had too many issues with commitment – always leaving people's wallets!",
+              "Why did the penny apply for a job? It wanted to make some cents!",
+              "What's a financial planner's favorite game? Hide and seek – because they love finding hidden expenses!",
+              "How do you make a small fortune in the stock market? Start with a large fortune!",
+              "Why did the piggy bank go to the doctor? It was feeling a little 'bankrupt'!",
+              "What's a mathematician's favorite type of money? Square root currency – it always makes sense!",
+              "Why do cashiers always look so calm? Because they know how to stay balanced!",
+              "Why did the banker switch careers? He lost interest!",
+              "Why did the coin take a vacation? It needed to 'unwind'!",
+              "How do you become a millionaire? Start as a billionaire and then invest in your favorite hobby until you're left with a million dollars!",
+              "What did one dollar say to the other dollar? 'You're worth a lot to me.'",
+              "Why did the scarecrow become a successful banker? Because he was outstanding in his field!",
+              "What's a vampire's favorite type of money? Blood money!",
+              "Why did the computer keep putting money in the blender? It wanted to make liquid assets!",
+              "Why was the math book sad? It had too many problems!",
+              "What do you call a rich elf? Welfy!",
+              "Why did the wallet go to therapy? It had too many emotional attachments!",
+              "What's a financial wizard's favorite spell? Abra-cadabra, now your debt's gone!",
+              "Why don't scientists trust atoms? Because they make up everything – even financial excuses!",
+              "Why was the math book looking sad? It had too many problems!",
+              "What do you call a coin that works in a bank? A change agent!",
+              "Why did the accountant break up with the calculator? It just couldn't count on it anymore!",
+              "Why did the credit card go to therapy? It had too much debt baggage!",
+              "What's a hedge fund manager's favorite type of plant? A money tree!",
+              "Why did the investor bring a ladder to the bank? To check out their high-interest rates!",
+              "Why did the accountant go broke? They lost their balance!",
+              "Why did the dollar go to school? To get a little 'cents' of education!",
+              "Why did the coin go to the doctor? It felt a bit 'off'!",
+              "What do you call a bear with no money? A 'bear' left!",
+              "Why did the wallet apply for a loan? It wanted to expand its 'pocket' size!",
+            ];
+            const randomJoke = moneyJokes.sort(() => Math.random() - 0.5)[0];
+            alert(randomJoke);
+          }}
+        >
+          {randomEmoji2}
+        </div>
+        <div className="border-t  border-green-800 flex-grow" />
+      </div>
       <div style={{ marginTop: 5 }}>
         <ResultsTable
           totalMoneyOut={sumOfMoneyOut}
@@ -169,6 +231,8 @@ const LeftSide = (props: {
           whatToShow={["TOTAL_MONEY_LEFT", "DAYS_UNTIL_NEXT_PAYCHECK"]}
         />
       </div>
+      <br />
+      <br />
     </>
   );
 };
