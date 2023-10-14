5370458db8c236fd7ce23c7d95c2d9d43a17ad19
Abdullah Hashim <Abdullah97hashim@gmail.com>
2023-10-14 12:38:07 +0300

    Add temp version of save to file system

--- src/app/homepage.tsx
+++ src/app/homepage.tsx
@@ -2,11 +2,66 @@ import {
   CalendarDaysIcon,
   IdentificationIcon,
 } from "@heroicons/react/20/solid";
-import { useNavigate } from "react-router-dom";
-import { Suspense } from "react";
+import { NavigateFunction, useNavigate } from "react-router-dom";
+import { Suspense, useState } from "react";
 import LoadingSpinner from "components/loading";
 import { useAuth0 } from "@auth0/auth0-react";
+import { CloudIcon, DocumentIcon } from "@heroicons/react/24/solid";
+import FileSystemStorage from "app/money-tracker/storage-types/file-system-storage";
 
+export const MonthBlock = (
+  month: string,
+  currentMonth: boolean,
+  link: string,
+  navigate: NavigateFunction,
+) => {
+  return (
+    // on hover, change the border color
+    <div
+      key={month}
+      className={[
+        "border border-gray-200 rounded-md p-4 hover:border-gray-400 cursor-pointer " +
+          "transition duration-500 ease-in-out transform " +
+          "hover:-translate-y-1 hover:scale-100 hover:shadow-lg hover:bg-gray-100",
+      ].join(" ")}
+      onClick={() => navigate(link)}
+    >
+      {currentMonth && (
+        <div
+          className={[
+            "rounded-full w-16 h-16 flex items-center justify-center " +
+              "bg-gray-200 text-gray-500",
+          ].join(" ")}
+        >
+          <CalendarDaysIcon />
+        </div>
+      )}
+      {!currentMonth && (
+        <div
+          className={[
+            "rounded-full w-16 h-16 flex items-center justify-center " +
+              "bg-gray-200 text-gray-500",
+          ].join(" ")}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8 text-gray-500"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              // eslint-disable-next-line max-len
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 011-1h4a1 1 0 110 2H8a1 1 0 01-1-1z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+      )}
+      <div className={"text-center mt-4 text-gray-500"}>{month} Salary</div>
+    </div>
+  );
+};
 const Homepage = () => {
   // create blocks for each month
   // from January to current month
@@ -14,59 +69,7 @@ const Homepage = () => {
   // tailwind css grid
   // heroicons for the icons
   const navigate = useNavigate();
-  const MonthBlock = (
-    month: string,
-    year: string,
-    currentMonth: boolean,
-    link: string,
-  ) => {
-    return (
-      // on hover, change the border color
-      <div
-        key={month}
-        className={[
-          "border border-gray-200 rounded-md p-4 hover:border-gray-400 cursor-pointer " +
-            "transition duration-500 ease-in-out transform " +
-            "hover:-translate-y-1 hover:scale-100 hover:shadow-lg hover:bg-gray-100",
-        ].join(" ")}
-        onClick={() => navigate(link)}
-      >
-        {currentMonth && (
-          <div
-            className={[
-              "rounded-full w-16 h-16 flex items-center justify-center " +
-                "bg-gray-200 text-gray-500",
-            ].join(" ")}
-          >
-            <CalendarDaysIcon />
-          </div>
-        )}
-        {!currentMonth && (
-          <div
-            className={[
-              "rounded-full w-16 h-16 flex items-center justify-center " +
-                "bg-gray-200 text-gray-500",
-            ].join(" ")}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-8 w-8 text-gray-500"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                // eslint-disable-next-line max-len
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 011-1h4a1 1 0 110 2H8a1 1 0 01-1-1z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-        )}
-        <div className={"text-center mt-4 text-gray-500"}>{month} Salary</div>
-      </div>
-    );
-  };
+
   const currentMonth = new Date().getMonth();
   const currentYear = new Date().getFullYear();
   const months = [8, 9]
@@ -77,13 +80,10 @@ const Homepage = () => {
       });
       const isCurrentMonth = month === currentMonth;
       const monthLink = `/money-tracker/${currentYear}/${month + 1}`;
-      return MonthBlock(
-        monthName,
-        currentYear.toString(),
-        isCurrentMonth,
-        monthLink,
-      );
+      return MonthBlock(monthName, isCurrentMonth, monthLink, navigate);
     });
+
+  const [selectedStorageType, setSelectedStorageType] = useState("cloud");
   const { isAuthenticated } = useAuth0();
   if (!isAuthenticated)
     return (
@@ -115,19 +115,78 @@ const Homepage = () => {
     );
   return (
     <Suspense fallback={<LoadingSpinner />}>
+      {/*Cloud or File based storage*/}
       <div className={"mx-auto mt-8 w-3/4"}>
-        <div className={"flex flex-row justify-between"}>
-          <div className={"text-2xl font-semibold"}>
-            Money Tracker
-            <div className={"text-gray-400"}>{currentYear}</div>
+        {/* Ask the user to select the storage type*/}
+        {/* If the user selects cloud storage show list of months and years*/}
+        {/* If the user selects file storage show list of files*/}
+        {/* If the user selects file storage and there are no files, show a message*/}
+        {/* If the user selects file storage and there are files, show list of files*/}
+        <div className={"text-2xl font-semibold"}>
+          {/*Make things next to each other*/}
+          <div className={"text-gray-600 flex flex-col"}>
+            Please select the storage type
           </div>
         </div>
-        <hr />
-        <br />
-
-        <div className={"grid grid-cols-3 gap-4"}>{months}</div>
+        {/*  make them next to each other with a space between them*/}
+        <div className={"flex flex-row content-start space-x-4"}>
+          <label className="label cursor-pointer">
+            <input
+              type="radio"
+              name="radio-10"
+              className="radio checked:bg-blue-500 mr-2"
+              checked={selectedStorageType === "cloud"}
+              onChange={() => setSelectedStorageType("cloud")}
+            />
+            <CloudIcon className={"w-6 h-6"} />
+            <span className="label-text">Cloud Storage</span>
+          </label>
+          <label className="label cursor-pointer">
+            <input
+              type="radio"
+              name="radio-10"
+              className="radio checked:bg-red-500 mr-2"
+              checked={selectedStorageType === "file"}
+              onChange={() => setSelectedStorageType("file")}
+            />
+            <DocumentIcon className={"w-6 h-6"} />
+            <span className="label-text">File Storage</span>
+          </label>
+        </div>
+      </div>
+      <div className={"mx-auto mt-8 w-3/4"}>
+        {selectedStorageType === "cloud" && (
+          <CloudMonths currentYear={currentYear} months={months} />
+        )}
+        {selectedStorageType === "file" && <FileSystemStorage />}
       </div>
     </Suspense>
   );
 };
+
+function CloudMonths({
+  currentYear,
+  months,
+}: {
+  currentYear: number;
+  months: JSX.Element[];
+}) {
+  return (
+    <>
+      <div className={"flex flex-row justify-between"}>
+        <div className={"text-2xl font-semibold"}>
+          Money Tracker
+          <div className={"text-gray-400"}>{currentYear}</div>
+        </div>
+      </div>
+      <hr />
+      <br />
+
+      <div className={"grid grid-cols-3 gap-4"}>
+        {months.map((month) => month)}
+      </div>
+    </>
+  );
+}
+
 export default Homepage;
---
+++ src/app/money-tracker/storage-types/file-system-storage.tsx
@@ -0,0 +1,104 @@
+import { useState } from "react";
+import { MonthBlock } from "app/homepage";
+import { useNavigate } from "react-router-dom";
+import { FolderOpenIcon } from "@heroicons/react/24/solid";
+
+function FileSystemStorage() {
+  const [directoryHandle, setDirectoryHandle] =
+    useState<FileSystemDirectoryHandle | null>(null); // DirectoryHandle
+  const navigate = useNavigate();
+
+  const currentMonth = new Date().getMonth();
+  const currentYear = new Date().getFullYear();
+  const months = [8, 9]
+    .map((n) => n - 1)
+    .map((month) => {
+      const monthName = new Date(0, month).toLocaleString("default", {
+        month: "long",
+      });
+      const isCurrentMonth = month === currentMonth;
+      const monthLink = `/money-tracker/${currentYear}/${month + 1}`;
+      return MonthBlock(monthName, isCurrentMonth, monthLink, navigate);
+    });
+
+  async function selectDirectory() {
+    const dirOptions: DirectoryPickerOptions = {
+      mode: "readwrite",
+      startIn: "documents",
+    };
+    const dirHandle = await window.showDirectoryPicker(dirOptions);
+    // make sure every month has a file
+    // if not create it
+    // if it does, read it
+    for (const month of months) {
+      await dirHandle.getFileHandle(`${month.key}.json`, {
+        create: true,
+      });
+    }
+    setDirectoryHandle(dirHandle);
+  }
+
+  return (
+    <>
+      <button className="btn btn-neutral" onClick={selectDirectory}>
+        <FolderOpenIcon className="h-5 w-5 mr-2" />
+        Select Directory Please
+      </button>
+      {/*print directory path and details*/}
+
+      {directoryHandle && (
+        <div>
+          <div className="text-left mt-4 text-gray-900">
+            <p>
+              Directory Name:
+              <span className={"kbd kbd-sm cursor-default"}>
+                {directoryHandle.name}
+              </span>
+            </p>
+            <p>
+              Access Mode:
+              <span className={"kbd kbd-sm cursor-default"}>Read / Write</span>
+            </p>
+          </div>
+          <br />
+          <div className="grid grid-cols-2 gap-4">
+            {months.map((month) => (
+              <div key={month.key} className="text-center">
+                {month}
+                <button
+                  className="btn"
+                  onClick={async () => {
+                    //   write dummy data to the file for the current month
+                    const fileHandle = await directoryHandle.getFileHandle(
+                      `${month.key}.json`,
+                      {
+                        create: true,
+                      },
+                    );
+                    const writable = await fileHandle.createWritable();
+                    await writable.write(
+                      JSON.stringify(
+                        {
+                          month: month.key,
+                          year: currentYear,
+                          data: new Date().toISOString(),
+                        },
+                        null,
+                        2,
+                      ),
+                    );
+                    await writable.close();
+                  }}
+                >
+                  test
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default FileSystemStorage;
